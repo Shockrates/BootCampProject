5678f5bb657fe7ead7c3a27e1a0e726a1768846a
ShockRate <blackhound05@yahoo.gr>
2025-09-30 06:16:19 +0300

    Auth file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Dashboard from './components/Dashboard/Dashboard'
 import About from './components/About/About'
 import MoviePage from './components/MoviePage/MoviePage'
+import Auth from './components/Auth/Auth'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
 
 // import './App.css'
 
@@ -28,6 +31,10 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/about" element={<About />} />
           <Route path="/movie/:id" element={<MoviePage />} />
+          <Route path="/auth" element={<Auth />} >
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
         </Routes>
       </main>
     </div>
---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const Auth = () => {
+    return (
+        <div>
+            <h1>Auth</h1>
+            <Outlet />
+        </div>
+
+    )
+}
+
+export default Auth
