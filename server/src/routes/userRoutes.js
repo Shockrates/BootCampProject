44add1171d30b71ef7e429e1d8bd285f8e7fdb42
kchatz97 <xatkat@gmail.com>
2025-10-04 22:14:23 +0300

    login returns user

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -21,7 +21,7 @@ const { email, password } = req.body;
     }
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ message: 'Login successful', token });
+    res.json({ message: 'Login successful', user });
 
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
