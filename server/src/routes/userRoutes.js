cfa435c0f037a01d1d7b959d39eb0b72ee04e470
kchatz97 <xatkat@gmail.com>
2025-10-05 09:57:34 +0300

    created userController

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,61 +1,11 @@
 import express from "express";
 import User from "../config/models/User.js";
-import bcrypt from 'bcrypt';
-import jwt from "jsonwebtoken";
+import { loginUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/login", async (req, res) => {
-const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    
-    if (!isMatch) {
-      return res.status(400).json({ message: 'Invalid credentials' });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    const { password: _, ...userWithoutPassword } = user.toObject();
-
-    res.json({ message: 'Login successful', user: userWithoutPassword, token });
-
-  } catch (err) {
-    res.status(500).json({ message: "Server error", error: err.message });
-  }
-      
-});
-
-
-router.post("/register", async  (req, res) => {
-  const { username, email, password, age } = req.body;
-
-  try {
-    if (!username || !email || !password || !age) {
-      return res.status(400).json({ message: 'All fields are required' });
-    }
-
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-      return res.status(400).json({ message: 'User already exists' });
-    }
-
-    const usernameExists = await User.findOne({ username });
-    if (usernameExists) {
-      return res.status(400).json({ message: 'User with that username already exists' });
-    }
-
-    const newUser = await User.create({ username, email, password , age});
-    res.status(201).json({ message: 'User registered successfully', user: newUser });
-  } catch (err) {
-    res.status(500).json({ err });
-  }
-});
+router.post("/login", loginUser);
+router.post("/register", registerUser);
 
 // router.get("/:id", getNoteById);
 // router.post("/", createNote);
