a000a78da0784840d75ece1c1c330441a087a659
InnerVision <matthewmanaras@gmail.com>
2025-10-15 00:34:58 +0300

    Correction

--- server/src/routes/watchedMovieRoutes.js
+++ server/src/routes/watchedMovieRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import Movie from '../config/models/WatchedMovie.js';
-import { createWatchedMovie } from '../controllers/watchedMovieController.js';
+import { createWatchedMovie, watchedByUser, getAllWatchedMovies } from '../controllers/watchedMovieController.js';
 
 const router = express.Router();
 
-router.post("/createWatchedMovie", createWatchedMovie); 
+router.post("/createWatchedMovie", createWatchedMovie);  // create a watched movie entry
+router.get("/watchedByUser", watchedByUser); // get watched movies by user
+router.get("/getAllWatchedMovies", getAllWatchedMovies); // get all watched movies
+
 
 export default router; // ES Module export
