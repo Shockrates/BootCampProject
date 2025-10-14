57c0b6be0216f5991c869f6dffa5ed989d96d4b0
InnerVision <matthewmanaras@gmail.com>
2025-10-15 00:27:14 +0300

    Created watchedByUser route and function

--- server/src/controllers/watchedMovieController.js
+++ server/src/controllers/watchedMovieController.js
@@ -1,5 +1,8 @@
+import Movie from "../config/models/Movie.js";
 import WatchedMovie from "../config/models/WatchedMovie.js"
 
+
+// Controller function to create a watched movie entry
 export async function createWatchedMovie (req,res) {
     try {
         const { userId,movieId,rating,review,watchedAt} = req.body;
@@ -14,3 +17,23 @@ export async function createWatchedMovie (req,res) {
     }
 }
 
+// Controller function to get watched movies by user  
+export async function watchedByUser(req, res) {
+    try {
+        const { user } = req.query; 
+        if (!user) {                                                        
+        return res.status(400).json({ message: "User ID is required" });        // αν δεν υπαρχει καθολου user id, μονο τοτε λεει οτι ειναι απαραιτητο. 
+        }                                                                       //to do : validate user by checking if it exists in User collection ( Matthew )
+
+        const watchedMovies = await WatchedMovie.find({ userId: user }).lean();
+        if (!watchedMovies || watchedMovies.length === 0) {
+            return res.status(404).json({ message: "No watched movies found for this user" });
+        }
+
+
+        res.status(200).json(watchedMovies);
+    } catch (error) {       
+        console.error("Error in getWatchedMoviesByUser controller", error);
+        res.status(500).json({message:"Internal server error"});
+    } 
+}
--- server/src/routes/watchedMovieRoutes.js
+++ server/src/routes/watchedMovieRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import Movie from '../config/models/WatchedMovie.js';
-import { createWatchedMovie } from '../controllers/watchedMovieController.js';
+import { createWatchedMovie, watchedByUser } from '../controllers/watchedMovieController.js';
 
 const router = express.Router();
 
-router.post("/createWatchedMovie", createWatchedMovie); 
+router.post("/createWatchedMovie", createWatchedMovie); // create a watched movie entry
+router.get("/watchedByUser", watchedByUser); // get watched movies by user
 
 export default router; // ES Module export
