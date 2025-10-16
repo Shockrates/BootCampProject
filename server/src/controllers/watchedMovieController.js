e8bc0022c1739f74f98483bcc192a9c06af88101
ShockRate <blackhound05@yahoo.gr>
2025-10-16 21:42:20 +0300

    Populated db with dummy reviews, some more testing code

--- server/src/controllers/watchedMovieController.js
+++ server/src/controllers/watchedMovieController.js
@@ -3,27 +3,27 @@ import WatchedMovie from "../config/models/WatchedMovie.js"
 
 
 // Controller function to create a watched movie entry
-export async function createWatchedMovie (req,res) {
+export async function createWatchedMovie(req, res) {
     try {
-        const {userId, movieId, rating, review, watchedAt} = req.body;
-        const watchedMovie = new WatchedMovie({userId,movieId,rating,review,watchedAt});
-        
+        const { userId, movieId, rating, review, watchedAt } = req.body;
+        const watchedMovie = new WatchedMovie({ userId, movieId, rating, review, watchedAt });
+
         const savedWatchedMovie = await watchedMovie.save();
-        res.status(200).json({savedWatchedMovie});
+        res.status(200).json({ savedWatchedMovie });
     } catch (error) {
         console.error("Error in create Watched Movie controller", error);
-        res.status(500).json({message:"Internal server error"});
+        res.status(500).json({ message: "Internal server error" });
     }
 }
 
-export async function getAllWatchedMovies(req,res){
-    try{
-        const watchedMovies = (await WatchedMovie.find().sort({createdAt:-1})); //-1 will sort in desc. order (newest first)
+export async function getAllWatchedMovies(req, res) {
+    try {
+        const watchedMovies = (await WatchedMovie.find().sort({ createdAt: -1 })); //-1 will sort in desc. order (newest first)
         res.status(200).json(watchedMovies);
     }
-    catch(error){
+    catch (error) {
         console.error("Error in getAllWatchedMovies controller", error);
-        res.status(500).json({message:"Internal server error"});
+        res.status(500).json({ message: "Internal server error" });
     }
 
 }
@@ -31,9 +31,9 @@ export async function getAllWatchedMovies(req,res){
 // Controller function to get watched movies by user  
 export async function watchedByUser(req, res) {
     try {
-        const { user } = req.query; 
-        if (!user) {                                                        
-        return res.status(400).json({ message: "User ID is required" });        // αν δεν υπαρχει καθολου user id, μονο τοτε λεει οτι ειναι απαραιτητο. 
+        const { user } = req.query;
+        if (!user) {
+            return res.status(400).json({ message: "User ID is required" });        // αν δεν υπαρχει καθολου user id, μονο τοτε λεει οτι ειναι απαραιτητο. 
         }                                                                       //to do : validate user by checking if it exists in User collection ( Matthew )
 
         const watchedMovies = await WatchedMovie.find({ userId: user }).lean();
@@ -43,8 +43,8 @@ export async function watchedByUser(req, res) {
 
 
         res.status(200).json(watchedMovies);
-    } catch (error) {       
+    } catch (error) {
         console.error("Error in getWatchedMoviesByUser controller", error);
-        res.status(500).json({message:"Internal server error"});
-    } 
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import User from "../config/models/User.js";
 import Movie from "../config/models/Movie.js";
-import { getUser} from "../controllers/userController.js";
+import WatchedMovie from "../config/models/WatchedMovie.js"
+import { getUser } from "../controllers/userController.js";
 import { loginUser, registerUser } from "../controllers/authController.js";
 
 const router = express.Router();
@@ -18,19 +19,33 @@ router.get("/user/:id", getUser);
 // router.delete("/:id", deleteNote);
 
 //ROUTES FOR TESTING PURPOSES (TO BE DELETED OR BETTER IMPLEMENTED BY BACKEND, ¨οπως νομίζεται καλύτερα :) )
-router.get("/usertest", async(req,res) => {
-        try {
-            const users = await User.find().limit(36);
-
-            if(!users) return res.status(404).json({message:"User not found"});
-            
-            res.status(200).json(users);
-        } catch (error) {
-            console.error("Error:", error);
-            res.status(500).json({message:"Internal server error"});
-        }
+router.get("/usertest", async (req, res) => {
+    try {
+        const users = await Movie.find();
+        if (!users) return res.status(404).json({ message: "User not found" });
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Internal server error" });
     }
+}
 );
+
+router.get("/reviewsTest", async (req, res) => {
+    try {
+        const watchedMovies = (await WatchedMovie.find()
+            //TESTING CODE, MODIFY AS YOU SEE FIT
+            .populate({ path: 'movieId', select: 'title' })
+            .populate({ path: 'userId', select: 'username email' })
+            //END OF TESTING CODE
+            .sort({ createdAt: -1 })); //-1 will sort in desc. order (newest first)
+        res.status(200).json(watchedMovies);
+    }
+    catch (error) {
+        console.error("Error in getAllWatchedMovies controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
 
 // //END OF TESTING CODE
 
