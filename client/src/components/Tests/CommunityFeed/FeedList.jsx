24a2ac6f240b15792766deace378b7e8bb261682
Shockrates <blackhound55@gmail.com>
2025-10-20 14:44:31 +0300

    Updated feedlist and feedlisItem

--- client/src/components/Tests/CommunityFeed/FeedList.jsx
+++ client/src/components/Tests/CommunityFeed/FeedList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Outlet, Navigate, Link } from 'react-router-dom'
-import { IoIosStar } from "react-icons/io";
-import { FaRegBookmark } from "react-icons/fa";
+import FeedListItem from './FeedListItem'
+
 
 const FeedList = ({ reviews }) => {
   if (!reviews || reviews.length === 0) return <p>No reviews available.</p>
@@ -11,50 +10,9 @@ const FeedList = ({ reviews }) => {
 
     <div>{
       /* Reads reviews */
-      reviews.map((r, index) => (
+      reviews.map((review, index) => (
 
-        <div className="flex flex-col border-b-2  text-slate-50 bg-stone-900 m-4 p-2 " key={index}>
-          <div className="review-items flex flex-col sm:flex-row">
-            <div className="flex flex-col items-end m-2 gap-2">
-              <p> {new Date(r.watchedAt).toLocaleDateString()}</p>
-              <FaRegBookmark />
-              <div className="flex flex-row gap-1">
-                {Array.from({ length: 5 }).map((_, index) => (
-                  <IoIosStar size={20} key={index} color='#D26D15' />
-                ))}
-
-              </div>
-
-            </div>
-            <div className="review-movie inline-block max-w-[200px]">
-
-              <img src={r.movieId?.poster_url} alt={r.movieId?.title} title={r.movieId?.title} loading='lazy'
-                onError={(e) => {
-                  e.currentTarget.src = "/no_poster.svg";
-                  e.currentTarget.alt = "Poster not available";
-                }}
-                className="block w-full h-auto shadow-sm"
-              />
-              <div
-                className="bg-[#D26D15] text-white text-sm font-medium px-3 py-2 break-words top-0"
-              >
-                {r.movieId?.title}
-              </div>
-            </div>
-            <div className="flex w-full items-end">
-              <div className="user-review flex flex-col m-2 w-full h-1/2 shadow-sm border-2 p-2 items-start">
-                <Link to="/">
-                  <h3>
-                    {r.userId.username},{r.userId.age}
-                  </h3>
-                </Link>
-                {/* <Link to={`/profile/${r.userId._id}`}><h3>{r.userId.username} </h3> </Link> */}
-                <p>{r.review}</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
+        <FeedListItem review={review} index={index}/>
 
       ))
     }</div>
