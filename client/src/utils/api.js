0f0b4e780f09c7be29cdac9336663f2a4dcc695a
ShockRate <blackhound05@yahoo.gr>
2025-10-21 21:34:40 +0300

    Sorry

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,8 +7,8 @@
 //  * - Logs errors to the console if the fetch request fails.
 //  */
 
-//const URI = "https://bootcampproject-production.up.railway.app";
-const URI = "http://localhost:3000";
+const URI = "https://bootcampproject-production.up.railway.app";
+// const URI = "http://localhost:3000";
 
 /**
  * Attempts to log in a user by checking email and password against the JSON server.
