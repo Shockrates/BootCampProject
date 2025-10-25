ab99319fb44b2ab6770504dc7c9c8f95aa297791
kchatz97 <xatkat@gmail.com>
2025-10-26 00:04:58 +0300

    updateReview (auth check needed)

--- server/src/controllers/reviewCommentController.js
+++ server/src/controllers/reviewCommentController.js
@@ -43,7 +43,6 @@ export async function createReviewComment(req, res) {
 
 }
 
-
 // Controller function to get ReviewComments by movieId
 export async function getReviewCommentsByWatchedMovie(req, res) {
     try {
@@ -70,6 +69,44 @@ export async function getReviewCommentsByWatchedMovie(req, res) {
         res.status(200).json(reviewComments);
 
             //5. Catch block
+    } catch (error) {
+        console.error("Error in get Review Comments by movie controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }   
+}
+
+
+// Controller function to Update ReviewComment by commentId
+export async function updateReviewCommentsByCommentId(req, res) {
+    //θελω ελεγχο ότι θέλει να αλλάξει comment Που είναι δικό του 
+    try {
+        const {comment, userId}= req.body;
+        const {commentId}  = req.params;
+
+        if (!comment || !userId) {
+            return res.status(400).json({ message: "Provide a string updated review" });
+        }
+        if (!commentId) {
+            return res.status(400).json({ message: "Comment ID is required" });
+        }
+        // 1 . Validate ObjectId
+        if (!mongoose.Types.ObjectId.isValid(commentId)) {
+            return res.status(400).json({ message: "Invalid commentId format" });
+        }
+        // 2. Check if commentId exists and update it
+        const updatedReviewComments = await ReviewComment.findOneAndUpdate({_id: commentId, commenterId:userId},{comment},{new:true})
+            .populate({ path: 'commenterId', select: 'username email' })
+            .lean();    
+            console.log("Review Comments fetched:", updatedReviewComments);
+        //3. If no comments found
+        if (!updatedReviewComments || updatedReviewComments.length === 0) {
+            return res.status(200).json({ message: "No review comments found for this watched movie" });
+        }
+
+        //4. Return the review comments
+        res.status(200).json(updatedReviewComments);
+
+        //5. Catch block
     } catch (error) {
         console.error("Error in get Review Comments by movie controller", error);
         res.status(500).json({ message: "Internal server error" });
--- server/src/routes/reviewCommentRoutes.js
+++ server/src/routes/reviewCommentRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createReviewComment,getReviewCommentsByWatchedMovie} from '../controllers/reviewCommentController.js';
+import {createReviewComment,getReviewCommentsByWatchedMovie,updateReviewCommentsByCommentId} from '../controllers/reviewCommentController.js';
 
 const router = express.Router();
 
 router.post("/createReviewComment", createReviewComment);  // create a review comment entry
 router.get("/getReviewCommentsByWatchedMovie/:watchedMovieId", getReviewCommentsByWatchedMovie); // get review comments by movieId
+router.put("/UpdateReviewCommentsByCommentId/:commentId", updateReviewCommentsByCommentId); // get review comments by movieId
 
 export default router; // ES Module export
