ed042f8114f280406229b0257af6c222b341069b
kchatz97 <xatkat@gmail.com>
2025-10-30 23:21:54 +0200

    About Page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -13,21 +13,6 @@ export default function About() {
         navigate('/imdb-top-movies'); // redirect to the login page
     };
 
-    // // Fetch 4 images from backend
-    // useEffect(() => {
-    //     const fetchTopRated = async () => {
-    //         try {
-    //             const response = await fetch('/getAllMovies'); // your backend endpoint
-    //             const data = await response.json();
-    //             setImages(data.slice(0, 4)); // take only 4 images
-    //         } catch (error) {
-    //             console.error('Error fetching images:', error);
-    //         }
-    //     };
-
-    //     fetchTopRated();
-    // }, []);
-
     const [movies, setMovies] = useState([])
 
     /**
@@ -39,7 +24,7 @@ export default function About() {
         let mounted = true;
         const loadMovies = async () => {
             try {
-                const movies = await fetchTopMovies(4, 0);
+                const movies = await fetchTopMovies();
                 if (!mounted) return;
                 setMovies(movies);
             } catch (error) {
@@ -88,18 +73,6 @@ export default function About() {
                 {/* Image Grid */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                     <MoviesTable movies={movies} />
-                    {/* {images.map((img, index) => (
-                        <div key={index} className="overflow-hidden rounded-lg shadow-lg">
-                            <img
-                                src={img.url} // backend returns "url"
-                                alt={img.title || `Movie ${index + 1}`}
-                                className="w-full h-64 object-cover hover:scale-105 transition-transform duration-300"
-                            />
-                            {img.title && (
-                                <div className="p-2 text-center font-semibold">{img.title}</div>
-                            )}
-                        </div>
-                    ))} */}
                 </div>
             </div>
         </div>
