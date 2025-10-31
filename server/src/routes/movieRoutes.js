94254ce68f4ce8b187b043b90b1ef0a9e97edfc5
Shockrates <blackhound55@gmail.com>
2025-10-31 15:28:52 +0200

    Added Route for Top rated movies

--- server/src/routes/movieRoutes.js
+++ server/src/routes/movieRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Movie from '../config/models/Movie.js';
-import { getAllMovies, getMovie, searchMovies, get4Movies } from '../controllers/movieController.js';
+import { getAllMovies, getMovie, searchMovies, get4Movies, getTopXMovies } from '../controllers/movieController.js';
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get("/getAllMovies", getAllMovies); // returns an array of 36 movies ( fo
 router.get("/searchMovies", searchMovies); // req a "searchTerm" STRING || returns movies that match the search term in their title
 router.get("/movie", getMovie);
 router.get("/get4movies", get4Movies);
+router.get('/getTopMovies/:limit', getTopXMovies); //Use this format /getTopXMovies/:limit?genre=:genre
 
 
 export default router; // ES Module export
