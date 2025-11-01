aacb88524064ba8c0dfda0eb883334ae0d65462a
ShockRate <blackhound05@yahoo.gr>
2025-11-01 11:44:36 +0200

    Changed Routes for Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,8 +36,8 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="movie/:id" element={<MoviePage />} />
           {/* <Route path='profile' element={<Profile />} /> */}
-          {/*<Route path='profile/:id' element={<Profile />} />*/}
-          <Route path='profile/:id' element={<TestProfile />} />
+          <Route path='profile/:id' element={<Profile />} />
+          {/* <Route path='profile/:id' element={<TestProfile />} /> */}
         </Route>
         <Route element={<PublicRoute />} >
           <Route index element={<CommunityFeed />} />
